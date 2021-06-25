652be42a5cae7bd63f53ef08750136735276d972
Edlâneo Júlio <edlanioj@gmail.com>
2021-06-25 22:40:20 +0100

    add routes to app and config fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import {
+  useFonts,
+  Rajdhani_700Bold,
+  Rajdhani_500Medium,
+} from '@expo-google-fonts/rajdhani';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
+import { Background } from './src/components/Background';
+import { Routes } from './src/routes';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar backgroundColor="transparent" translucent style="light" />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
